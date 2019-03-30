cffb6e9f0a750ec9a5e917cf2313af1777eefb0b
DannythePastaMan <dgomez0998@gmail.com>
2019-03-29 22:24:16 -0600

    Getting the hang of this

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,28 @@
 pragma solidity >=0.4.2;
 
 
-contract Election {
+contract Election 
+{
 
-    string public candidate;
+    struct Candidate
+    {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    constructor () public {
-        candidate = "Candidate 1";
+    mapping(uint=>Candidate) public candidates;
+    uint public candidatesCount;
+
+    constructor () public 
+    {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }   
+
+    function addCandidate(string memory _name) private
+    {
+        candidatesCount++;
+        candidates [candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
